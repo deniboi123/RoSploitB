fa9ee5f7b8f3bf27a9821fda4f14c6bbcea95181
deniboi123 <58114191+deniboi123@users.noreply.github.com>
2021-06-13 15:37:54 +0100

    UI update for uncompiled.cs

--- uncompiled.cs
+++ uncompiled.cs
@@ -10,6 +10,10 @@ using System.Windows.Forms;
 using System.Runtime.InteropServices;
 using WeAreDevs_API;
 using System.IO;
+using System.Security.Cryptography;
+using System.Net.Http;
+using System.Net;
+using System.Diagnostics;
 
 namespace RoSploit
 {
@@ -40,25 +44,54 @@ namespace RoSploit
             }
         }
 
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        
+
+        private void Form1_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+
+        private void panel1_Click(object sender,  EventArgs e)
+        {
+            
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+           
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+           
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
    
-
-        private void button6_Click(object sender, EventArgs e)
-        {
-            fastColoredTextBox1.Text = "";
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            if (api.isAPIAttached())
-            {
-                button2.Text = "ATTACHED";
-            }
-            else
-            {
-                api.LaunchExploit();
-            }
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
             api.SendLuaScript(fastColoredTextBox1.Text);
@@ -79,14 +112,60 @@ namespace RoSploit
             File.WriteAllText(@name, fastColoredTextBox1.Text);
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        
+
+      
+
+      
+   
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Process[] pname = Process.GetProcessesByName("RobloxPlayerBeta");
+            if (pname.Length == 0)
+            {
+                Form2 myNewForm = new Form2();
+
+                myNewForm.Show();
+            }
+            else
+            {
+
+                foreach (var process in Process.GetProcessesByName("RobloxPlayerBeta"))
+                {
+                    process.Kill();
+                }
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            api.LaunchExploit();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            api.SendLuaScript(fastColoredTextBox1.Text);
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
         {
             openFileDialog1.ShowDialog();
         }
 
-        private void button4_Click(object sender, EventArgs e)
+        private void bunifuButton1_Click(object sender, EventArgs e)
         {
-            saveFileDialog1.ShowDialog();
+            System.Diagnostics.Process.Start("http://wearedevs.net");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
         }
     }
 }
